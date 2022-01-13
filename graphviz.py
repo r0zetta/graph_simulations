576cc1867245b03b58fe7b7ca5a076bb84b4d3a9
Andy Patel <rozetta@gmail.com>
2022-01-13 14:11:23 +0200

    Added new features

--- graphviz.py
+++ graphviz.py
@@ -9,13 +9,19 @@ import math
 
 class GraphViz:
     def __init__(self, inter, degree, initial_pos=None,
-                 mag_factor=1.0, scaling=5.0, gravity=20.0):
+                 mag_factor=1.0, scaling=5.0, gravity=20.0, iterations=200,
+                 eadjust=0.5, expand=0.3, zoom=[[0.0,0.0],[1.0,1.0]], auto_zoom=True):
         self.inter = inter
         self.degree = degree
         self.initial_pos = initial_pos
         self.mag_factor = mag_factor
         self.scaling = scaling
         self.gravity = gravity
+        self.eadjust = eadjust
+        self.expand = expand
+        self.zoom = zoom
+        self.auto_zoom = auto_zoom
+        self.iterations = iterations
         self.canvas_width = int(1200 * self.mag_factor)
         self.canvas_height = int(1200 * self.mag_factor)
         self.color_palette = ((0, 131, 182), (255, 75, 0), (32, 198, 0), (255, 84, 255),
@@ -128,13 +134,90 @@ class GraphViz:
         self.draw.text((xoff, yoff), label, fill=(255,255,255), font=font)
 
     def convert_coords(self, x, y):
-        coords_width = self.max_x - self.min_x
-        coords_height = self.max_y - self.min_y
-        width_ratio = (self.canvas_width-(self.mag_factor*100))/coords_width
-        height_ratio = (self.canvas_height-(self.mag_factor*100))/coords_height
-        new_x = (x - self.min_x)*width_ratio
-        new_y = (y - self.min_y)*height_ratio
+        zxmin, zymin = self.zoom[0]
+        zxmax, zymax = self.zoom[1]
+        zwidth = zxmax - zxmin
+        zheight = zymax - zymin
+        coords_width = (self.max_x - self.min_x) * zwidth
+        coords_height = (self.max_y - self.min_y) * zheight
+        width_ratio = self.canvas_width/coords_width
+        height_ratio = self.canvas_height/coords_height
+        new_x = (x - self.min_x)*width_ratio - (self.canvas_width * (zxmin/zwidth))
+        new_y = (y - self.min_y)*height_ratio - (self.canvas_height * (zymin/zheight))
         return new_x, new_y
+
+    def expand_graph(self, pos):
+        max_x = max([c[0] for x, c in pos.items()])
+        min_x = min([c[0] for x, c in pos.items()])
+        max_y = max([c[1] for x, c in pos.items()])
+        min_y = min([c[1] for x, c in pos.items()])
+        mid_x = (max_x - min_x)/2
+        mid_y = (max_y - min_y)/2
+        mid_p = (mid_x, mid_y)
+        new_pos = {}
+        for sn, coords in pos.items():
+            dist = self.distance(mid_p, coords)
+            angle = self.angle(mid_p, coords)
+            new_coords = self.move_point(coords, dist * self.expand, angle)
+            new_pos[sn] = new_coords
+        return new_pos
+
+    def coords_in_rect(self, coords, xl, yl, xh, yh):
+        cx, cy = coords
+        if cx >= xl and cx <= xh and cy >= yl and cy <= yh:
+            return True
+        return False
+
+    def get_bounds(self, var):
+        steps = len(var)
+        le = 0
+        re = 0
+        is_dense = False
+        for i in range(steps):
+            if is_dense == False:
+                re += 1
+                if var[i] < 10:
+                    le += 1
+                else:
+                    is_dense = True
+            else:
+                if var[i] > 10:
+                    re += 1
+        return le, re
+
+    def autozoom(self, pos):
+        max_x = int(max([c[0] for x, c in pos.items()]))
+        min_x = int(min([c[0] for x, c in pos.items()]))
+        max_y = int(max([c[1] for x, c in pos.items()]))
+        min_y = int(min([c[1] for x, c in pos.items()]))
+        steps = 10
+        sw = int((max_x - min_x)/steps)
+        sh = int((max_y - min_y)/steps)
+        # Left to right sweep
+        xsweep = Counter()
+        for i, x in enumerate(range(min_x, max_x, sw)):
+            xsweep[i] = 0
+            for sn, coords in pos.items():
+                xl = x
+                xh = x + sw
+                yl = min_y
+                yh = max_y
+                if self.coords_in_rect(coords, xl, yl, xh, yh) is True:
+                    xsweep[i] += 1
+        # Top to bottom sweep
+        ysweep = Counter()
+        for i, y in enumerate(range(min_y, max_y, sh)):
+            ysweep[i] = 0
+            for sn, coords in pos.items():
+                xl = min_x
+                xh = max_x
+                yl = y
+                yh = y + sh
+                if self.coords_in_rect(coords, xl, yl, xh, yh) is True:
+                    ysweep[i] += 1
+        lx, ux = self.get_bounds(xsweep)
+        ly, uy = self.get_bounds(ysweep)
+        self.zoom = [[lx*0.1, ly*0.1], [ux*0.1, uy*0.1]]
 
     def get_stats(self):
         mapping = []
@@ -156,7 +239,11 @@ class GraphViz:
             self.clusters[mod].append(node)
 
         FA2 = ForceAtlas2(self.G, scalingRatio=self.scaling, gravity=self.gravity, verbose=False)
-        pos = FA2.forceatlas2_networkx_layout(self.G, pos=self.initial_pos, iterations=100)
+        pos = FA2.forceatlas2_networkx_layout(self.G, pos=self.initial_pos, iterations=self.iterations)
+        if self.expand > 0:
+            pos = self.expand_graph(pos)
+        if self.auto_zoom == True:
+            self.autozoom(pos)
         self.max_x = max([c[0] for x, c in pos.items()])
         self.min_x = min([c[0] for x, c in pos.items()])
         self.max_y = max([c[1] for x, c in pos.items()])
@@ -169,8 +256,10 @@ class GraphViz:
             self.positions[sn] = [newx, newy]
 
         self.max_degree = max([c for x, c in self.degree.items()])
+        self.min_degree = min([c for x, c in self.degree.items()])
         self.node_sizes = dict([(node, 5+(5*(degree/self.max_degree))) for node, degree in self.degree.items()])
         self.max_node_size = max([c for x, c in self.node_sizes.items()])
+        self.min_node_size = min([c for x, c in self.node_sizes.items()])
 
         self.modularity_class = {}
         for community_number, community in self.clusters.items():
@@ -187,8 +276,10 @@ class GraphViz:
             for target, weight in targets.items():
                 w = int((weight*3)/self.max_weight)
                 mod = self.modularity_class[source]
-                color = self.color_palette[mod]
-                df = 0.25 + (0.2*w)
+                color = (128, 128, 128)
+                if mod < len(self.color_palette):
+                    color = self.color_palette[mod]
+                df = 0.25 + (self.eadjust * w)
                 color = self.adjust(color, df)
                 sp = self.positions[source]
                 tp = self.positions[target]
@@ -203,7 +294,9 @@ class GraphViz:
             mod = 1
             if sn in self.modularity_class:
                 mod = self.modularity_class[sn]
-            color = self.color_palette[mod]
+            color = (128, 128, 128)
+            if mod < len(self.color_palette):
+                color = self.color_palette[mod]
             font_size = 1 + int(20 * (s/self.max_node_size))
             self.draw_node(xpos, ypos, s, color)
             self.draw_label(xpos, ypos, sn, font_size)
@@ -226,7 +319,6 @@ class GraphViz:
             angles[sn] = a
         max_d = max([c for x, c in dists.items()])
         mean_d = np.mean([c for x, c in dists.items()])
-        print(max_d, mean_d)
         if mean_d < 10:
             iml.append(g2.make_graphviz())
             return iml
@@ -239,6 +331,7 @@ class GraphViz:
                 a = angles[sn]
                 new_p = self.move_point(p, d, a)
                 self.positions[sn] = new_p
+            print("Interpolating " + str(step+1) + "/" + str(num_steps))
             iml.append(self.make_graphviz())
         iml.append(g2.make_graphviz())
         return iml
